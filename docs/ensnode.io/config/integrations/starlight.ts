a99764ec461bc78dad47c69cd11ac260a586aab7
Jamie Barton <jamie@notrab.dev>
2025-03-06 20:56:10 +0000

    feat(ensnode.io): add sidebar topics to documentation (#333)

--- docs/ensnode.io/config/integrations/starlight.ts
+++ docs/ensnode.io/config/integrations/starlight.ts
@@ -1,10 +1,116 @@
 import AstroStarlight from "@astrojs/starlight";
 import { type AstroIntegration } from "astro";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 import starlightThemeRapide from "starlight-theme-rapide";
 
 export function starlight(): AstroIntegration {
   return AstroStarlight({
-    plugins: [starlightThemeRapide()],
+    plugins: [
+      starlightThemeRapide(),
+      starlightSidebarTopics([
+        {
+          label: "ENSNode",
+          link: "/ensnode/",
+          icon: "star",
+          items: [
+            {
+              label: "Overview",
+              items: [
+                {
+                  label: "Quickstart",
+                  link: "/ensnode",
+                },
+                {
+                  label: "What is ENSNode?",
+                  link: "/ensnode/concepts/what-is-ensnode",
+                },
+                {
+                  label: "What is ENS Subgraph?",
+                  link: "/ensnode/concepts/what-is-the-ens-subgraph",
+                },
+              ],
+            },
+            {
+              label: "Using ENSNode",
+              collapsed: false,
+              autogenerate: { directory: "ensnode/usage" },
+            },
+            {
+              label: "Deploying ENSNode",
+              collapsed: true,
+              autogenerate: { directory: "ensnode/deploying" },
+            },
+            {
+              label: "Local ENSNode",
+              collapsed: true,
+              autogenerate: { directory: "ensnode/running" },
+            },
+            {
+              label: "Contributing",
+              collapsed: true,
+              autogenerate: { directory: "ensnode/contributing" },
+            },
+            {
+              label: "Reference",
+              collapsed: true,
+              autogenerate: { directory: "ensnode/reference" },
+            },
+          ],
+        },
+        {
+          label: "ENSRainbow",
+          link: "/ensrainbow/",
+          icon: "star",
+          items: [
+            {
+              label: "Overview",
+              items: [
+                {
+                  label: "Quickstart",
+                  link: "/ensrainbow",
+                },
+              ],
+            },
+            {
+              label: "Using ENSRainbow",
+              collapsed: false,
+              autogenerate: { directory: "ensrainbow/usage" },
+            },
+            {
+              label: "Deploying ENSRainbow",
+              collapsed: true,
+              autogenerate: { directory: "ensrainbow/deploying" },
+            },
+            {
+              label: "Contributing",
+              collapsed: true,
+              autogenerate: { directory: "ensrainbow/contributing" },
+            },
+          ],
+        },
+        {
+          label: "ENSAdmin",
+          link: "/ensadmin/",
+          icon: "star",
+          items: [
+            {
+              label: "Overview",
+              items: [
+                {
+                  label: "Quickstart",
+                  link: "/ensadmin",
+                },
+              ],
+            },
+            {
+              label: "Contributing",
+              collapsed: true,
+              autogenerate: { directory: "ensadmin/contributing" },
+            },
+          ],
+        },
+      ]),
+    ],
     title: "ENSNode",
     logo: {
       light: "./src/assets/light-logo.svg",
@@ -14,65 +120,6 @@ export function starlight(): AstroIntegration {
     social: {
       github: "https://github.com/namehash/ensnode",
     },
-    sidebar: [
-      {
-        label: "ENSNode",
-        collapsed: false,
-        items: [
-          {
-            label: "Using ENSNode",
-            collapsed: false,
-            autogenerate: { directory: "ensnode/usage" },
-          },
-          {
-            label: "Understanding ENSNode",
-            collapsed: true,
-            autogenerate: { directory: "ensnode/understanding" },
-          },
-          {
-            label: "Deploying ENSNode",
-            collapsed: true,
-            autogenerate: { directory: "ensnode/deploying" },
-          },
-          {
-            label: "Local ENSNode",
-            collapsed: true,
-            autogenerate: { directory: "ensnode/running" },
-          },
-          {
-            label: "Contributing",
-            collapsed: true,
-            autogenerate: { directory: "ensnode/contributing" },
-          },
-          {
-            label: "Reference",
-            collapsed: true,
-            autogenerate: { directory: "ensnode/reference" },
-          },
-        ],
-      },
-      {
-        label: "ENSRainbow",
-        collapsed: false,
-        items: [
-          {
-            label: "Using ENSRainbow",
-            collapsed: false,
-            autogenerate: { directory: "ensrainbow/usage" },
-          },
-          {
-            label: "Deploying ENSRainbow",
-            collapsed: true,
-            autogenerate: { directory: "ensrainbow/deploying" },
-          },
-          {
-            label: "Contributing",
-            collapsed: true,
-            autogenerate: { directory: "ensrainbow/contributing" },
-          },
-        ],
-      },
-    ],
     editLink: {
       baseUrl: "https://github.com/namehash/ensnode/edit/main/docs/ensnode.io",
     },
